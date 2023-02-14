247b3a76d0fbce40232eb71915fe75ca11db2406
icyywaa <nazoriiellii002@gmail.com>
2023-02-14 14:21:04 +0300

    Background

---
+++ blogger/src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import background from "../assets/background.jpg";
+
+const Background = () => {
+  return (
+    <div className="h-screen w-screen bg-fixed overflow-auto">
+      <img src={background} alt="background main page" />
+    </div>
+  );
+};
+
+export default Background;
