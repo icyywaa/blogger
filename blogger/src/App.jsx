79cf4f1eadf44a819f4885b72f6da1e56f51324e
icyywaa <nazoriiellii002@gmail.com>
2023-02-14 21:01:27 +0300

    added navbar

--- blogger/src/App.jsx
+++ blogger/src/App.jsx
@@ -1,8 +1,10 @@
 import Background from "./components/Background";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
+      <Navbar />
       <Background />
     </div>
   );
