1d1dbc51db38adea06b6116239814a0bff969ee8
icyywaa <nazoriiellii002@gmail.com>
2023-02-16 12:49:53 +0300

    parent div, gradient for background

--- blogger/src/components/Background.jsx
+++ blogger/src/components/Background.jsx
@@ -3,8 +3,14 @@ import background from "../assets/background.jpg";
 
 const Background = () => {
   return (
-    <div className="h-screen w-screen bg-fixed overflow-auto">
-      <img src={background} alt="background main page" />
+    <div>
+      <div className="bg-gradient-to-tr from-gray-200 to-gray-600 h-full w-full">
+        <img
+          src={background}
+          className="w-full h-full object-cover mix-blend-overlay "
+          alt="background main page"
+        />
+      </div>
     </div>
   );
 };
