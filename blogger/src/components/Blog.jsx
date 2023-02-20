6b230e7db0c2560768d977be940398f18098edb3
icyywaa <nazoriiellii002@gmail.com>
2023-02-20 16:29:29 +0300

    Blog/ cards

--- blogger/src/components/Blog.jsx
+++ blogger/src/components/Blog.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import Card from "./Card";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="bg-slate-300 ">
+      <div>
+        <h2 className="text-center pt-24 uppercase font-[Poppins] font-bold text-2xl">
+          AI generated
+        </h2>
+        <h6 className="text-center text-sm font-[Poppins]">
+          Pick a category and get started
+        </h6>
+      </div>
+      <Card
+        title="Mysterous Stories"
+        imageURL="https://images.unsplash.com/photo-1566754844421-9bc834baf4a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+        body="As the night fell, the moon cast an eerie glow upon the abandoned mansion. The wind howled through the broken windows, rustling the tattered curtains and stirring up dust that had long settled on the floor. It was said that the mansion was cursed, haunted by the ghosts of its former inhabitants who had met a tragic end.
+        Many had tried to unravel the secrets of the mansion, but none had succeeded. Some claimed to have heard faint whispers in the darkness, while others reported seeing shadowy figures lurking in the corners of their eyes. But no one dared to enter the mansion alone, for fear of never returning.
+        As the years passed, the mansion remained a mystery, a dark and foreboding presence that loomed over the small town below. Some said that the mansion was a portal to another world, while others believed that it held the key to unlocking unimaginable power.
+        Whatever the truth may be, the mansion remained a tantalizing enigma, drawing in curious adventurers and thrill-seekers from all corners of the world. And as long as it stood, the mysteries of the mansion would continue to haunt the minds of those who dared to venture near."
+      />
+      <Card
+        title="Short Novals"
+        imageURL="https://images.unsplash.com/photo-1614707267537-b85aaf00c4b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+        body="Title: The Art of Falling in Love
+
+        Chapter 1: The Chance Encounter
+        
+        Sarah had always been a hopeless romantic, believing in the power of fate and destiny. So when she met Daniel by chance at a local coffee shop, she couldn't help but feel that it was meant to be.
+        
+        He was tall, with piercing blue eyes and a warm smile that immediately put her at ease. They struck up a conversation over their shared love of art, and Sarah was smitten by the way Daniel spoke about his passion for painting.
+        
+        As they parted ways, Sarah couldn't shake the feeling that she had just met someone special. She spent the next few days replaying their conversation in her mind and imagining what it would be like to see him again.
+        
+        Chapter 2: The First Date
+        
+        Luckily for Sarah, fate had other plans. Daniel reached out to her a few days later, asking her out on a date to a local art museum. Sarah eagerly accepted, feeling a flutter of excitement in her stomach.
+        
+        The date was everything Sarah had hoped for and more. They strolled through the galleries, marveling at the beautiful artwork and sharing stories about their own experiences with creativity. Sarah felt like she could talk to Daniel about anything, and she found herself opening up to him in a way she never had with anyone else.
+        
+        As the evening came to a close, Daniel took Sarah's hand and looked deep into her eyes. 'I know this might sound crazy,' he said, 'but I feel like I've known you my whole life.'
+        
+        Sarah felt her heart skip a beat. 'I feel the same way,' she whispered, and they shared their first kiss.
+        
+        Chapter 3: The Honeymoon Phase
+        
+        In the weeks that followed, Sarah and Daniel were inseparable. They explored the city together, tried new restaurants, and spent long nights talking about everything under the sun.
+        
+        Sarah felt like she was living in a fairy tale. She had never felt so loved and cherished before, and she knew that Daniel was the one she had been waiting for.
+        
+        But as the honeymoon phase began to fade, Sarah started to notice some cracks in their relationship. Daniel was often distant, lost in his own thoughts, and Sarah couldn't help but wonder what he was hiding from her.
+        
+        Chapter 4: The Revelation
+        
+        One day, Sarah couldn't take it anymore. She confronted Daniel, demanding to know what was going on.
+        
+        At first, he hesitated, but then he took a deep breath and told her the truth: he had been diagnosed with a rare form of cancer a few months before they met. He had been in remission, but he had recently learned that the cancer had returned and was spreading quickly.
+        
+        Sarah felt like the ground had dropped out from under her. She couldn't imagine a life without Daniel, and she was devastated by the thought of losing him.
+        
+        But as they talked and cried together, Sarah realized that she was in this for the long haul. She loved Daniel, and she was willing to do whatever it took to be there for him, no matter what the future held.
+        
+        Chapter 5: The End and the Beginning
+        
+        In the months that followed, Sarah stood by Daniel's side as he fought his illness with courage and grace. They laughed, cried, and loved fiercely, knowing that every moment was precious.
+        
+        And even when Daniel passed away, Sarah knew that their love was not over. It lived on in her memories, in the artwork they had created together, and in the way she saw the world through his eyes.
+        
+        For Sarah, falling in love with Daniel had been a journey of discovery and loss, but also of hope and healing. She knew that the art of falling in love was not just about finding someone to share your life with, but about opening your heart to all"
+      />
+      <Card
+        title="'Longing For The Past'"
+        imageURL="https://images.unsplash.com/photo-1632610680757-753244cf2112?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+        body="Emily sat at her desk, staring at the blank piece of paper in front of her. She couldn't remember the last time she had written a letter by hand. Everything had become digital, from texting to emailing, and she felt a sense of loss at the thought that the art of writing hand-written letters had died.
+
+        She picked up her pen and began to write. The words flowed from her mind and onto the paper, each stroke of the pen filling her with a sense of nostalgia. She wrote about her day, her dreams, and her fears, and as she wrote, she felt like she was connecting with something deeper and more meaningful.
+        
+        When she finished, she folded the letter neatly and addressed it to her best friend who lived across the country. She placed it in an envelope, sealed it with a lick of the adhesive strip, and affixed a stamp to the corner.
+        
+        As she dropped the letter into the mailbox, Emily felt a pang of sadness. The mailbox, once filled with envelopes and postcards, was now mostly empty. The art of writing letters had slowly died, replaced by the speed and convenience of digital communication.
+        
+        But as she walked away from the mailbox, Emily felt a sense of hope. She knew that there were still people out there who appreciated the beauty and intimacy of hand-written letters, and she vowed to keep the art alive, one letter at a time."
+      />
+      <Card
+        title="Short Retellings of Disney Stories"
+        imageURL="https://images.unsplash.com/photo-1544070643-24128d1f6033?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+        body="Rapunzel is a fairy tale about a young girl with long, golden hair who is kept locked up in a tower by a wicked enchantress. The enchantress, who had kidnapped Rapunzel from her parents when she was a baby, visits her by climbing up her hair, which is so long that it reaches the ground.
+
+One day, a prince passes by the tower and hears Rapunzel singing. He is enchanted by her beautiful voice and decides to investigate. He discovers the tower and sees Rapunzel at the top, but he cannot climb up to her. He returns many times to visit her, and she eventually agrees to let down her hair so he can climb up.
+
+The prince and Rapunzel fall in love, but the enchantress discovers their secret meetings and punishes Rapunzel by cutting off her hair and banishing her to a remote location. The prince is heartbroken and wanders the wilderness, blinded by thorns, until he hears Rapunzel's voice singing a lullaby that he recognizes. Her tears heal his blindness, and they are reunited.
+
+The story ends happily, with the prince taking Rapunzel back to his kingdom and marrying her. The enchantress is punished for her wickedness, and Rapunzel is reunited with her true family."
+      />
+    </div>
+  );
 };
 
 export default Blog;
